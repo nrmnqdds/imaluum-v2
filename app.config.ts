a77cdbd7db0c36a79d8690306d4f5d958785900e
nrmnqdds <quddusnuriman@gmail.com>
2024-10-16 23:18:37 +0800

    :art: change folder structure

--- app.config.ts
+++ app.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from "@tanstack/start/config";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	vite: {
-		plugins: () => [
-			tsConfigPaths({
-				projects: ["./tsconfig.json"],
-			}),
-		],
-	},
+  server: {
+    preset: "cloudflare-pages",
+  },
+  vite: {
+    plugins: () => [
+      tsConfigPaths({
+        projects: ["./tsconfig.json"],
+      }),
+    ],
+  },
 });

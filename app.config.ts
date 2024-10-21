9aa2da298a457d70923c173b50a93afe54f2b35b
nrmnqdds <quddusnuriman@gmail.com>
2024-10-21 18:49:43 +0800

    :zap: prerender with nitro

--- app.config.ts
+++ app.config.ts
@@ -2,14 +2,29 @@ import { defineConfig } from "@tanstack/start/config";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  server: {
-    preset: "cloudflare-pages",
-  },
-  vite: {
-    plugins: [
-      tsConfigPaths({
-        projects: ["./tsconfig.json"],
-      }),
-    ],
-  },
+	server: {
+		preset: "cloudflare-pages",
+		prerender: {
+			routes: ["/"],
+			crawlLinks: true,
+		},
+	},
+	vite: {
+		build: {
+			chunkSizeWarningLimit: 100,
+			rollupOptions: {
+				onwarn(warning, warn) {
+					if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+						return;
+					}
+					warn(warning);
+				},
+			},
+		},
+		plugins: [
+			tsConfigPaths({
+				projects: ["./tsconfig.json"],
+			}),
+		],
+	},
 });

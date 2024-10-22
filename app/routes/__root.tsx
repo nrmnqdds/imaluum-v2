b8510500c10fc2dce9202348792f7adf2be92b66
nrmnqdds <quddusnuriman@gmail.com>
2024-10-23 01:01:59 +0800

    :green_heart: test ci

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -22,6 +22,14 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
 		],
 		component: RootComponent,
 		links: () => [{ rel: "stylesheet", href: appCss }],
+		scripts: () => [
+			{
+				src: "https://umami.mallam.chat/getinfo",
+				type: "module",
+				defer: true,
+				dataWebsiteId: "45609499-5b7c-451e-9683-4ef520bae636",
+			},
+		],
 	},
 );
 

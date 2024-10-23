79e7f10e5b601c5faf21f6d98694b5c900442026
nrmnqdds <quddusnuriman@gmail.com>
2024-10-23 13:31:48 +0800

    :bug: try fix cookie

--- app/components/landing-page/login-form.tsx
+++ app/components/landing-page/login-form.tsx
@@ -7,109 +7,111 @@ import { Input } from "~/components/shared/input";
 import useProfile from "~/hooks/use-profile";
 import useResult from "~/hooks/use-result";
 import useSchedule from "~/hooks/use-schedule";
-import { setCookie } from "vinxi/http";
+import { deleteCookie, setCookie } from "vinxi/http";
 import { request } from "undici";
 
 type TLoginResponse = {
-	status: number;
-	message: string;
-	data: {
-		username: string;
-		token: string;
-	};
+  status: number;
+  message: string;
+  data: {
+    username: string;
+    token: string;
+  };
 };
 
 type Credentials = {
-	username: string;
-	password: string;
+  username: string;
+  password: string;
 };
 
 const LoginForm = () => {
-	const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-	const { reset: ProfileReset, setProfile } = useProfile();
-	const { reset: ScheduleReset } = useSchedule();
-	const { reset: ResultReset } = useResult();
+  const { reset: ProfileReset, setProfile } = useProfile();
+  const { reset: ScheduleReset } = useSchedule();
+  const { reset: ResultReset } = useResult();
 
-	const router = useRouter();
+  const router = useRouter();
 
-	const loginUser = createServerFn("POST", async (credentials: Credentials) => {
-		const res = await request("https://api.nrmnqdds.com/api/login", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(credentials),
-		});
+  const loginUser = createServerFn("POST", async (credentials: Credentials) => {
+    const res = await request("https://api.nrmnqdds.com/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(credentials),
+    });
 
-		const json = (await res.body.json()) as unknown as TLoginResponse;
+    const json = (await res.body.json()) as unknown as TLoginResponse;
 
-		setCookie("MOD_AUTH_CAS", json.data.token, {
-			// Expires in 30 minutes
-			maxAge: 1800,
-		});
+    deleteCookie("MOD_AUTH_CAS");
 
-		return json;
-	});
+    setCookie("MOD_AUTH_CAS", json.data.token, {
+      // Expires in 30 minutes
+      expires: new Date(Date.now() + 30 * 60 * 1000),
+    });
 
-	const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		const form = new FormData(e.currentTarget);
+    return json;
+  });
 
-		const username = form.get("username") as string;
-		const password = form.get("password") as string;
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
 
-		try {
-			setIsLoading(true);
-			const res = await loginUser({ username, password });
+    const username = form.get("username") as string;
+    const password = form.get("password") as string;
 
-			ProfileReset();
-			ScheduleReset();
-			ResultReset();
+    try {
+      setIsLoading(true);
+      const res = await loginUser({ username, password });
 
-			if (res.status === 200 || res.status === 201) {
-				setProfile({
-					matric_no: res.data.username,
-					name: "",
-					image_url: "",
-				});
-				router.navigate({
-					to: "/dashboard",
-				});
-			} else {
-				console.log(res);
-				toast.error(res.message);
-			}
-		} catch (err) {
-			console.log(err);
-			toast.error("An error occurred. Please try again later.");
-		} finally {
-			setIsLoading(false);
-		}
-	};
+      ProfileReset();
+      ScheduleReset();
+      ResultReset();
 
-	return (
-		<form onSubmit={handleLogin} className="space-y-2 mt-10 w-fit">
-			<div className="flex items-center justify-center gap-3">
-				<Input
-					name="username"
-					placeholder="Matric Number"
-					disabled={isLoading}
-				/>
-				<Input
-					name="password"
-					placeholder="Password"
-					type="password"
-					disabled={isLoading}
-				/>
-			</div>
-			<Button type="submit" disabled={isLoading} className="float-right">
-				<span className="text-foreground">
-					{isLoading ? "Logging in" : "Log in"}
-				</span>
-			</Button>
-		</form>
-	);
+      if (res.status === 200 || res.status === 201) {
+        setProfile({
+          matric_no: res.data.username,
+          name: "",
+          image_url: "",
+        });
+        router.navigate({
+          to: "/dashboard",
+        });
+      } else {
+        console.log(res);
+        toast.error(res.message);
+      }
+    } catch (err) {
+      console.log(err);
+      toast.error("An error occurred. Please try again later.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin} className="space-y-2 mt-10 w-fit">
+      <div className="flex items-center justify-center gap-3">
+        <Input
+          name="username"
+          placeholder="Matric Number"
+          disabled={isLoading}
+        />
+        <Input
+          name="password"
+          placeholder="Password"
+          type="password"
+          disabled={isLoading}
+        />
+      </div>
+      <Button type="submit" disabled={isLoading} className="float-right">
+        <span className="text-foreground">
+          {isLoading ? "Logging in" : "Log in"}
+        </span>
+      </Button>
+    </form>
+  );
 };
 
 export default LoginForm;

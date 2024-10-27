b43c2f6a0379afbfe3e05cecd307848ba534c493
nrmnqdds <quddusnuriman@gmail.com>
2024-10-28 01:29:37 +0800

    :green_heart: ci again

--- app.config.ts
+++ app.config.ts
@@ -2,29 +2,29 @@ import { defineConfig } from "@tanstack/start/config";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	server: {
-		preset: "node-server",
-		prerender: {
-			routes: ["/"],
-			crawlLinks: true,
-		},
-	},
-	vite: {
-		build: {
-			chunkSizeWarningLimit: 100,
-			rollupOptions: {
-				onwarn(warning, warn) {
-					if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
-						return;
-					}
-					warn(warning);
-				},
-			},
-		},
-		plugins: [
-			tsConfigPaths({
-				projects: ["./tsconfig.json"],
-			}),
-		],
-	},
+  server: {
+    preset: "node-server",
+    prerender: {
+      routes: ["/"],
+      crawlLinks: true,
+    },
+  },
+  vite: {
+    build: {
+      chunkSizeWarningLimit: 100,
+      rollupOptions: {
+        onwarn(warning, warn) {
+          if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+            return;
+          }
+          warn(warning);
+        },
+      },
+    },
+    plugins: [
+      tsConfigPaths({
+        projects: ["./tsconfig.json"],
+      }),
+    ],
+  },
 });

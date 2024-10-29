3ae23f9bf95d51f76c5c4da7b3f81b891d8616b2
nrmnqdds <quddusnuriman@gmail.com>
2024-10-29 21:37:41 +0800

    :bug: fix file import

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -6,60 +6,60 @@ import appCss from "~/styles/app.css?url";
 import type { QueryClient } from "@tanstack/react-query";
 
 export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
-  {
-    meta: () => [
-      {
-        charSet: "utf-8",
-      },
-      {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1",
-      },
-      {
-        title: "Simplified i-Ma'luum",
-        description: "A simplified version of i-Ma'luum for students.",
-      },
-      {
-        rel: "icon",
-        href: "/favicon.ico",
-      },
-    ],
-    component: RootComponent,
-    links: () => [{ rel: "stylesheet", href: appCss }],
-    // scripts: () => [
-    // 	{
-    // 		src: "https://umami.mallam.chat/getinfo",
-    // 		type: "module",
-    // 		defer: true,
-    // 		datawebsiteid: "45609499-5b7c-451e-9683-4ef520bae636",
-    // 	},
-    // ],
-  },
+	{
+		meta: () => [
+			{
+				charSet: "utf-8",
+			},
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1",
+			},
+			{
+				title: "Simplified i-Ma'luum",
+				description: "A simplified version of i-Ma'luum for students.",
+			},
+			{
+				rel: "icon",
+				href: "/favicon.ico",
+			},
+		],
+		component: RootComponent,
+		links: () => [{ rel: "stylesheet", href: appCss }],
+		// scripts: () => [
+		// 	{
+		// 		src: "https://umami.mallam.chat/getinfo",
+		// 		type: "module",
+		// 		defer: true,
+		// 		datawebsiteid: "45609499-5b7c-451e-9683-4ef520bae636",
+		// 	},
+		// ],
+	},
 );
 
 function RootComponent() {
-  return (
-    <RootDocument>
-      <Outlet />
-    </RootDocument>
-  );
+	return (
+		<RootDocument>
+			<Outlet />
+		</RootDocument>
+	);
 }
 
 function RootDocument({ children }: { children: React.ReactNode }) {
-  return (
-    <Html lang="en">
-      <Head>
-        <Meta />
-      </Head>
-      <Body className="scroll-smooth">
-        <ThemeProvider attribute="class" defaultTheme="dark">
-          <main className="relative isolate overflow-hidden bg-card min-h-screen">
-            {children}
-          </main>
-          <ScrollRestoration />
-          <Scripts />
-        </ThemeProvider>
-      </Body>
-    </Html>
-  );
+	return (
+		<Html lang="en">
+			<Head>
+				<Meta />
+			</Head>
+			<Body className="scroll-smooth">
+				<ThemeProvider attribute="class" defaultTheme="dark">
+					<main className="relative isolate overflow-hidden bg-card min-h-screen">
+						{children}
+					</main>
+					<ScrollRestoration />
+					<Scripts />
+				</ThemeProvider>
+			</Body>
+		</Html>
+	);
 }

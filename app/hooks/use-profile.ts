6ea9ae8533b163126ed9e16f227ef5812ab2d43b
nrmnqdds <quddusnuriman@gmail.com>
2024-10-29 21:13:25 +0800

    :sparkles: show name

--- app/hooks/use-profile.ts
+++ app/hooks/use-profile.ts
@@ -3,23 +3,34 @@ import { createJSONStorage, persist } from "zustand/middleware";
 import type { StudentInfo } from "~/types/student";
 
 type ProfileType = {
-	profile: StudentInfo | null;
-	setProfile: (profile: StudentInfo) => void;
-	reset: () => void;
+  profile: StudentInfo;
+  setProfile: (profile: StudentInfo) => void;
+  reset: () => void;
 };
 
 const useProfile = create(
-	persist<ProfileType>(
-		(set) => ({
-			profile: null,
-			setProfile: (profile: StudentInfo) => set({ profile }),
-			reset: () => set({ profile: null }),
-		}),
-		{
-			name: "profile-storage",
-			storage: createJSONStorage(() => sessionStorage),
-		},
-	),
+  persist<ProfileType>(
+    (set) => ({
+      profile: {
+        image_url: "",
+        name: "",
+        matric_no: "",
+      },
+      setProfile: (profile: StudentInfo) => set({ profile }),
+      reset: () =>
+        set({
+          profile: {
+            image_url: "",
+            name: "",
+            matric_no: "",
+          },
+        }),
+    }),
+    {
+      name: "profile-storage",
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
 );
 
 export default useProfile;
